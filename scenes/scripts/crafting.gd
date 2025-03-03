61dbdfbbb5aa748a9dac35a2792d7858ea9819fc
herlyks <31139133+herlyks@users.noreply.github.com>
2025-03-03 18:45:36 +0700

    added craft multiple times at once via slider
    - craft multiple times at once via slider

--- scenes/scripts/crafting.gd
+++ scenes/scripts/crafting.gd
@@ -3,6 +3,9 @@ extends TabBar
 @onready var list = %RecipeItemList
 @onready var ingredient_vbox = %IngredientVBoxContainer
 @onready var craft_btn = %CraftButton
+@onready var amount_slider = %AmountHSlider
+
+var _craft_amount = 1
 
 var items = ItemLoader.load_crafting_items()
 var ingredients = []
@@ -53,14 +56,17 @@ func clear_ingredients():
 func player_has_ingredients():
 	var item_in_inventory = []
 	var player_inventory = Player.get_inventory()
+	var amounts = []
 	
 	if ingredients.size() == 0:
+		amount_slider.max_value = 1
 		return false
 	
 	for item in ingredients:
 		var filtered_inventory = player_inventory.filter(func(a): return item.id == a.id)
 		
 		if filtered_inventory.size() == 0:
+			amount_slider.max_value = 1
 			return false
 		
 		for player_item in filtered_inventory:
@@ -68,6 +74,10 @@ func player_has_ingredients():
 				item_in_inventory.append(true)
 			else:
 				item_in_inventory.append(false)
+		
+			amounts.append(floor(player_item.count / item.count))
+	
+	amount_slider.max_value = amounts.min()
 	
 	for i in item_in_inventory:
 		if i == false:
@@ -79,10 +89,16 @@ func player_has_ingredients():
 func _on_craft_button_pressed() -> void:
 	var index = list.get_selected_items()[0]
 	var crafted_item = list.get_item_metadata(index).duplicate()
+	crafted_item.count = get_craft_amount()
 	
 	for item in ingredients:
-		Player.remove_item(item)
-		Player.add_item(crafted_item)
+		var i = item.duplicate()
+		i.count = i.count * get_craft_amount()
+		
+		Player.remove_item(i)
+	
+	Player.add_item(crafted_item)
+	amount_slider.value = 1
 
 
 func set_craft_btn():
@@ -90,3 +106,16 @@ func set_craft_btn():
 		craft_btn.disabled = false
 	else:
 		craft_btn.disabled = true
+
+
+func _on_amount_h_slider_value_changed(value: float) -> void:
+	set_craft_amount(amount_slider.value)
+	craft_btn.text = "Craft " + str(get_craft_amount())
+
+
+func set_craft_amount(value):
+	_craft_amount = value
+
+
+func get_craft_amount():
+	return _craft_amount
